06351731d0bf29f7520064c8fa9be7723b65ba95
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2023-02-14 18:05:40 +0100

    Add user authentication

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const _ = require('lodash')
+const bcrypt = require('bcrypt')
 const Joi = require('joi')
 mongoose.set('strictQuery', true);
 
@@ -35,6 +36,8 @@ router.post('/', async (req, res) => {
   if(user) return res.status(400).send('User already exists')
 
   user = new User (_.pick(req.body, ['name', 'email', 'password']))
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(user.password, salt)
 
   await user.save()
 
