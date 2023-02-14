79837bc22af401b557b6b35ddb85c4b3286dfc3f
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2023-02-14 16:35:18 +0100

    Install Lodash

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const _ = require('lodash')
 const Joi = require('joi')
 mongoose.set('strictQuery', true);
 
@@ -27,55 +28,19 @@ const User = mongoose.model("User", new mongoose.Schema({
   }
 }))
 
-// router.post('/', async(req, res) => {
-//   const users = await Genre.find().sort('name')
-//   res.send(users)
-// })
-
-// router.get('/:id', async (req, res) => {
-//   const genre = await Genre.findById(req.params.id)
-//   if (!genre) return res.status(404).send('That genre does not exist')
-//   res.send(genre)
-// })
-
 router.post('/', async (req, res) => {
   const { error } = validateUser(req.body)
   if (error) return res.status(400).send(error.details[0].message)
   let user = await User.findOne({email: req.body.email})
   if(user) return res.status(400).send('User already exists')
 
-  user = new User ({
-    name:req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  })
+  user = new User (_.pick(req.body, ['name', 'email', 'password']))
 
   await user.save()
 
-  res.send(user)
+
+  res.send(_.pick(user, ['_id', 'name', 'email']))
 })
-
-// router.put('/:id', async (req,res) => {
-//   const { error } = validateGenre(req.body)
-//   if (error) return res.status(400).send(error.details[0].message)
-
-//   const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name},{
-//     new: true 
-//   })
-
-//   if (!genre) return res.status(404).send('That genre does not exist')
-
-//   res.send(genre)
-// })
-
-// router.delete('/:id', async (req, res) => {
-//   const genre = await Genre.findByIdAndRemove(req.params.id)
-
-//   if (!genre) return res.status(404).send('That genre does not exist')
-
-//   res.send(genre)
-
-// })
 
 function validateUser(user) {
   const schema = {
