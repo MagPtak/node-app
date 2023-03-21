363982b0f4fe476da9cdd64db1aa4368db44837a
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2023-03-21 12:05:43 +0100

    Add tests

--- startup/logging.js
+++ startup/logging.js
@@ -1,13 +1,11 @@
 module.exports = function() {
-
   process.on('uncaughtException', (ex) => {
     winston.error(ex.message, ex)
   })
-
   process.on('unhandledRejection', (ex) => {
     winston.error(ex.message, ex)
   })
-
-  winston.configure({transports: [new winston.transports.File({ filename: 'logfile.log' }) ]});
-
+  winston.configure({transports: [new winston.transports.File({ 
+    filename: 'logfile.log' 
+  }) ]});
 }
